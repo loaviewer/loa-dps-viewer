1a80b1f7e9bb40b322152e65b6f7380483f892df
loaviewer <dnjswjd10041@gmail.com>
2026-08-02 11:19:12 +0900

    Update server.js

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ function getNextApiKey() {
 
 const ALLOWED_ORIGINS = [
   "https://loaviewer.github.io",
-  // "http://127.0.0.1:5500",
-  // "http://localhost:5500",
+   "http://127.0.0.1:5500",
+    "http://localhost:5500",
 ];
 
 app.use(cors({ origin: ALLOWED_ORIGINS }));
